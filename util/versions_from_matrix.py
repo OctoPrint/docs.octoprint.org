c2c0381b51318876af5bc34ecd754d5ed2ddd3d8
Gina Häußge <gina@octoprint.org>
2021-05-03 16:32:12 +0200

    Code formatting & add fix encoding issues

--- util/versions_from_matrix.py
+++ util/versions_from_matrix.py
@@ -1,35 +1,38 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-__license__ = 'GNU Affero General Public License http://www.gnu.org/licenses/agpl.html'
+__license__ = "GNU Affero General Public License http://www.gnu.org/licenses/agpl.html"
 __copyright__ = "Copyright (C) 2020 The OctoPrint Project - Released under terms of the AGPLv3 License"
 
-import sys
 import json
+import sys
+
 import yaml
 
 if __name__ == "__main__":
-	workflow_path = sys.argv[1]
-	versions_path = sys.argv[2]
+    workflow_path = sys.argv[1]
+    versions_path = sys.argv[2]
 
-	with open(workflow_path, mode="r") as f:
-		data = yaml.safe_load(f)
+    with open(workflow_path, mode="r", encoding="utf8") as f:
+        data = yaml.safe_load(f)
 
-	matrix = data["jobs"]["build"]["strategy"]["matrix"]["include"]
-	print(repr(matrix))
+    matrix = data["jobs"]["build"]["strategy"]["matrix"]["include"]
+    print(repr(matrix))
 
-	versions = dict()
-	for version in matrix:
-		name = version["version"]
-		source = version["source"]
-		print("Adding {}/{} to versions.json".format(name, source))
+    versions = dict()
+    for version in matrix:
+        name = version["version"]
+        source = version["source"]
+        print("Adding {}/{} to versions.json".format(name, source))
 
-		versions[name] = dict(name=name,
-		                      url="https://docs.octoprint.org/en/{}".format(name),
-		                      is_released=source == "tags",
-		                      source=source)
+        versions[name] = dict(
+            name=name,
+            url="https://docs.octoprint.org/en/{}".format(name),
+            is_released=source == "tags",
+            source=source,
+        )
 
-	with open(versions_path, mode="w") as f:
-		json.dump(versions, f)
+    with open(versions_path, mode="w", encoding="utf8") as f:
+        json.dump(versions, f)
 
-	print("versions.json dumped at {}".format(versions_path))
+    print("versions.json dumped at {}".format(versions_path))
